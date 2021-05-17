39691b4f28433752448ae5dd131452b976a98c01
caglarcercinli <chaglar.ronin@gmail.com>
2021-05-17 10:24:19 +0200

    eerste push

---
+++ src/main/java/be/vdab/advancedtaken/AdvancedTakenApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.advancedtaken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedTakenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedTakenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/advancedtaken/AdvancedTakenApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.advancedtaken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedTakenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
